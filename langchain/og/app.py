9cfa75ec02ad32b1be8fc796e397225f15d8f824
Roger Martinez <roger.that.dev@gmail.com>
2024-09-14 16:44:43 -0700

    prompt templates

--- langchain/og/app.py
+++ langchain/og/app.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompt_values import ChatPromptValue
 
 load_dotenv()
 
@@ -28,3 +30,21 @@ print(parsedResult)
 # Chaining 
 chain = model | parser
 print(chain.invoke(messages))
+
+system_template: str = "Translate the following into {language}:"
+
+prompt_template: ChatPromptTemplate = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+result: ChatPromptValue = prompt_template.invoke({"language": "italian", "text": "hi"})
+
+print(result)
+
+## Print the messages in ChatPromptValue
+print("Messages:")
+print(result.to_messages())
+
+# Chain with a prompt template:
+chain = prompt_template | model | parser
+result = chain.invoke({"language": "italian", "text": "hi"})
