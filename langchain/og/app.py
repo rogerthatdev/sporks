cf02db93e652fa3fa7f020712fb6d9fda1922fd5
Roger Martinez <roger.that.dev@gmail.com>
2024-09-14 15:56:48 -0700

    add output parser code

--- langchain/og/app.py
+++ langchain/og/app.py
@@ -2,14 +2,15 @@ import os
 from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
 model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
 
 messages = [
-    SystemMessage(content=""),
-    HumanMessage(content="hi!")
+    SystemMessage(content="Translate the following from English into Italian"),
+    HumanMessage(content="hi!"),
 ]
 
 result:AIMessage = model.invoke(messages)
@@ -17,3 +18,9 @@ result:AIMessage = model.invoke(messages)
 for attr, value in result.__dict__.items():
     print(f"{attr}: {value}")
 
+
+# Output Parsers
+parser = StrOutputParser()
+result = model.invoke(messages)
+parsedResult = parser.invoke(result)
+print(parsedResult)
