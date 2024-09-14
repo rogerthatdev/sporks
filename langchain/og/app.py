c92a13e240997912ee1eb54cf94661a9003958ae
Roger Martinez <roger.that.dev@gmail.com>
2024-09-14 15:40:49 -0700

    use the model directly; print the values of AIMessage result

--- langchain/og/app.py
+++ langchain/og/app.py
@@ -1,4 +1,19 @@
 import os
 from dotenv import load_dotenv
+from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 load_dotenv()
+
+model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+
+messages = [
+    SystemMessage(content=""),
+    HumanMessage(content="hi!")
+]
+
+result:AIMessage = model.invoke(messages)
+
+for attr, value in result.__dict__.items():
+    print(f"{attr}: {value}")
+
