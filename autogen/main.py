63d6e96e0af87b3391b8988a3c95867d0cca5a4a
rogerthatdev <roger.that.dev@gmail.com>
2024-10-02 14:30:40 -0700

    add sample of 2 agents conversing with oneanother

--- autogen/main.py
+++ autogen/main.py
@@ -35,6 +35,37 @@ example_messages = [
     }
 ]
 
-reply = agent.generate_reply(messages=example_messages)
-print(reply)
+# reply = agent.generate_reply(messages=example_messages)
+# print(reply)
+
+# Roles and conversations
+
+cathy_config = {
+    "config_list": [
+        {"model": "gpt-4", 
+         "temperature": 0.9, 
+         "api_key": os.environ.get("OPENAI_API_KEY")}]}
+
+joe_config = {
+    "config_list": [
+        {"model": "gpt-4", 
+         "temperature": 0.7, 
+         "api_key": os.environ.get("OPENAI_API_KEY")}]}
+
+cathy = ConversableAgent(
+    "cathy",
+    system_message="Your name is Cathy and you are a part of a duo of comedians.",
+    llm_config=cathy_config,
+    human_input_mode="NEVER",  # Never ask for human input.
+)
+
+joe = ConversableAgent(
+    "joe",
+    system_message="Your name is Joe and you are a part of a duo of comedians.",
+    llm_config=joe_config,
+    human_input_mode="NEVER",  # Never ask for human input.
+)
+# use initiate_chat with max_turns as 2 to limit the conversation to 2 turns
+result = joe.initiate_chat(cathy, message="Cathy, tell me a joke.", max_turns=2)
+
 
